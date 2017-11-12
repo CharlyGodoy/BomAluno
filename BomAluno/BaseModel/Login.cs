e372b15b8211358f5072d19d93bd987a264fa02c
victor herzog <victorherzog17@hotmail.com>
2017-11-12 13:16:43 -0200

    Base model criada com os relacionamentos

---
+++ BomAluno/BaseModel/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseModel
+{
+	class Login
+	{
+		
+		public int LoginID { get; set; }
+
+		public string Nome { get; set; }
+
+		public string Usuario { get; set; }
+
+		public string Senha { get; set; }
+
+		public bool Ativo { get; set; }
+	}
+}
---
+++ BomAluno/BaseModel/Materia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseModel
+{
+	class Materia
+	{
+		public int MateriaID { get; set; }
+
+		public string Nome { get; set; }
+
+		public string Descricao { get; set; }
+
+		public bool Ativo { get; set; }
+
+		public int AtividadeID { get; set; }
+
+		public List<Atividade> _Atividade { get; set; }
+
+	}
+}
