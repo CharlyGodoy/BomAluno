0f062525f61183f1988583cbd1a6c8d85b773f5f
victor herzog <victorherzog17@hotmail.com>
2017-11-22 19:23:29 -0200

    controler materia

--- BomAlunoNew/BomAlunoNew/Controllers/MateriasController.cs
+++ BomAlunoNew/BomAlunoNew/Controllers/MateriasController.cs
@@ -53,6 +53,7 @@ namespace BomAlunoNew.Controllers
         {
             if (ModelState.IsValid)
             {
+                materia.Ativo = true;
                 db.Materias.Add(materia);
                 db.SaveChanges();
                 return RedirectToAction("Index");
